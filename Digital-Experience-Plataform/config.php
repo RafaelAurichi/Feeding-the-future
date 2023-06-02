ec15442dbdfcc5505dafff422aa43c81506100fa
rafael aurichi <rafael.aurichi@ouzzi.ag>
2023-06-02 11:57:06 -0300

    crud php

---
+++ Digital-Experience-Plataform/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'cadastro';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+?>
